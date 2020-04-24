6453ccb89581ee2d89c1148a2e5a0f3e04c22a80
RiinaMiichelle <sarina.m.colosimo@gmail.com>
2020-04-23 21:36:44 -0500

    finished up to step 15

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { Button, Table, TableHead, TableBody, TableRow, TableCell, TableContainer, Menu, MenuItem } from '@material-ui/core';
+import { MoreVert } from '@material-ui/icons';
 
 const Import = (props) => {
     // fill out this component
-
+    console.log(props.makes);
     return (
-        <p>Import Component</p>
+    <div>
+        <Button variant="contained" color="primary" onClick={props.fetchMakes}>Import</Button>
+        <TableContainer>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Id</TableCell>
+                        <TableCell>Make</TableCell>
+                        <TableCell>Actions<MoreVert></MoreVert></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {props.makes.map((make, index) => (
+                        <TableRow>
+                            <TableCell index={index}>{make.MakeId}</TableCell>
+                            <TableCell index={index}>{make.MakeName}</TableCell>
+                            <TableCell index={index}>{make.Actions}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+        <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+    </div>
     )
 }
 
--- src/containers/Import.js
+++ src/containers/Import.js
@@ -1,1 +1,21 @@
 // fill out this container
+import { connect } from 'react-redux'
+import Import from '../components/Import'
+import { fetchMakes } from '../redux/actions'
+import { deleteMake } from '../red/actions'
+
+const mapStateToProps = (state) => {
+  return {
+    makes: state.makes
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchMakes: () => dispatch(fetchMakes()),
+    deleteMake: () => dispatch(deleteMake())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Import)
+
